aa232bb7830c90c4fa8a68d7334a625880a6245c
Alvin Pratama <113691381+palveeen22@users.noreply.github.com>
2025-12-10 11:44:42 +0300

    feat: config and packages added for projects

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,53 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-export default eslintConfig;
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^react", "^@?\\w"],
+            ["^(?!\\.\\.|\\./|@/).*\\u0000$", "^@/.*\\u0000$"],
+            ["^@/app"],
+            ["^@/features"],
+            ["^@/widgets"],
+            ["^@/entities"],
+            ["^@/assets", "^@/shared"],
+            ["^\\../", "^\\./", "^@/pages"],
+          ],
+        },
+      ],
+    },
+  },
+]
+
+export default eslintConfig
